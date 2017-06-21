12b4285ef87a6bc8088bd7aab9b487785ccbb8eb
cwilliams3579 <clevelandwilliams3579@gmail.com>
2017-06-21 12:04:55 -0400

    setup devise

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -40,3 +40,4 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 # setting per server overrides global ssh_options
 
 # fetch(:default_env).merge!(rails_env: :production)
+# config.action_mailer.default_url_options = { host: 'gist2cents.com', protocol: 'https' }
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { confirmations: 'confirmations' }
   root to: 'visitors#index'
 end
